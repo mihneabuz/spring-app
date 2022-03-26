0933c685b6c75b58ac190cc367a9b47eab5bf41a
Niko18500 <andreinicolescu3@gmail.com>
2022-03-26 13:22:14 +0200

    Agent connect + disconnect

---
+++ src/main/java/com/model/agent/ConnectAgentRequest.java
@@ -0,0 +1,29 @@
+package com.model.agent;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter @Setter @ToString
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ConnectAgentRequest {
+
+    @JsonProperty(required = true)
+    public String ip;
+
+    @JsonProperty(required = true)
+    public String port;
+
+    @JsonProperty(required = true)
+    public String root;
+
+    @JsonCreator
+    public ConnectAgentRequest(String ip, String port, String root) {
+        this.ip = ip;
+        this.port = port;
+        this.root = root;
+    }
+}
